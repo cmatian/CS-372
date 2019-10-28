7c4b10eef1eb3a467d8056a6358acbdef8039b64
Chris Matian <chris.matian@race.com>
2019-10-28 16:16:04 -0700

    Update python server

--- Python-Client-Server-Learning/TCPServer.py
+++ Python-Client-Server-Learning/TCPServer.py
@@ -3,29 +3,29 @@ from socket import *
 serverPort = 12001
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
-serverSocket.bind(('127.0.0.1', serverPort))
+serverSocket.bind(('', serverPort))
 
 serverSocket.listen(1)
 
-print('The Server is ready to receive')
-nt = '\0'
-flag = 1
+print('The Server is ready to receive...')
+nullterm = '\0'
 
+flag = 1
 connectionSocket, addr = serverSocket.accept()
-while addr and flag == 1:
+serverSocket.close()
+while flag == 1:
     inMessage = connectionSocket.recv(2048).decode()
 
     print(f"{inMessage}")
-    outMessage = f"Server Received: {inMessage}\0"
+    outMessage = f"Server Received: {inMessage}{nullterm}"
 
     # Send message back
     connectionSocket.send(outMessage.encode())
 
     # Set flag to exit if message is "./quit"
-    if inMessage[:6] == "./quit":
+    if "./quit" in inMessage:
         print("Quit command received - terminating socket and connection.")
         flag = 0
-
 connectionSocket.close()
 
 
